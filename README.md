9d482ee0358539c8bf73b32cf08d935d796724a7
NovemberDev <46710246+NovemberDev@users.noreply.github.com>
2021-08-05 18:36:56 +0200

    oops

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# novemberdev-rpg-godot-tutorial
+# Godot RPG Tutorial by NovemberDev
  
-Under construction
+This is a 2D rpg I built for a video on my channel:
+
+ ## Screenshot
+![Screenshot](repo_images/rpg_0.png)
+
+## Video
+[![NovemberDev RPG Tutorial](https://img.youtube.com/vi/gIllVrLRDxM/0.jpg)](https://www.youtube.com/watch?v=gIllVrLRDxM)
+
+Link: https://www.youtube.com/watch?v=gIllVrLRDxM
--- scripts/PLAYER.gd
+++ scripts/PLAYER.gd
@@ -19,13 +19,18 @@ func _process(delta):
 	direction_input = Vector2.ZERO
 	direction_input.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
 	direction_input.y = int(Input.is_key_pressed(KEY_W)) - int(Input.is_key_pressed(KEY_S))
+	
 	if direction_input != Vector2.ZERO:
 		linear_velocity = lerp(linear_velocity, direction_input * WALK_SPEED, delta * 5.0)
+		$MODEL/AnimationTree.set("parameters/state/current", 1)
 		current_orientation = direction_input
 	else:
 		linear_velocity = lerp(linear_velocity, Vector2.ZERO, delta * FRICTION)
+		$MODEL/AnimationTree.set("parameters/state/current", 0)
 	
 	move_and_slide(linear_velocity + current_knockback)
+	$MODEL/AnimationTree.set("parameters/move/blend_position", current_orientation)
+	$MODEL/AnimationTree.set("parameters/idle/blend_position", current_orientation)
 	
 	if Input.is_action_just_pressed("attack") and is_weapon_equipped():
 		$MODEL/AnimationTree.set("parameters/attack/active", true)
@@ -47,12 +52,16 @@ func on_interact_body_exited(body):
 func on_item_equip():
 	if GAMESTATE.left_hand_item_name == "Knife":
 		if !$MODEL/LEFTHAND.visible:
+			var d = current_orientation * Vector2(2.0, 1.0) - Vector2(float($MODEL/LEFTHAND.visible), 0)
+			$MODEL/AnimationTree.set("parameters/attack_bs/blend_position", d)
 			$MODEL/FX/AnimationPlayer.play("action")
 			$MODEL/LEFTHAND.visible = true
 	else:
 		$MODEL/LEFTHAND.visible = false
 	if GAMESTATE.right_hand_item_name == "Knife":
 		if !$MODEL/RIGHTHAND.visible:
+			var d = current_orientation * Vector2(2.0, 1.0) + Vector2(float($MODEL/RIGHTHAND.visible), 0)
+			$MODEL/AnimationTree.set("parameters/attack_bs/blend_position", d)
 			$MODEL/FX/AnimationPlayer.play("action")
 			$MODEL/RIGHTHAND.visible = true
 	else:
